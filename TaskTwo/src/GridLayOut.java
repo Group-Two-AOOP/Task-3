d373f8d400c5b0b209e15b75b29eb50ac88d98cc
Humra <humuransimbi@gmail.com>
2026-03-20 22:49:44 +0300

    Add files via upload

---
+++ TaskTwo/src/GridLayOut.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class GridLayOut {
+    JFrame frame;
+    JPanel PanelOne,PanelTwo, PanelThree;
+
+        public GridLayOut() {
+            this.createFrame();
+        }
+
+        // Method 1: Create JFrame
+        public JFrame createFrame() {
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            frame = new JFrame("GridLayout");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH); // Maximize window
+            frame.setLayout(new GridLayout(1, 3)); // 1 row, 3 columns
+
+            frame.add(this.CreateJPanelOne());
+            frame.add(this.CreateJPanelTwo());
+            frame.add(this.CreateJPanelThree());
+
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            return frame;
+        }
+
+        public JPanel CreateJPanelOne() {
+            PanelOne = new JPanel();
+            Border JPanelOneBorder = BorderFactory.createTitledBorder("JPANEL ONE");
+            PanelOne.setBorder(JPanelOneBorder);
+            PanelOne.setBackground(Color.CYAN);
+            return PanelOne;
+        }
+
+        public JPanel CreateJPanelTwo(){
+            PanelTwo = new JPanel();
+            Border JPanelTwoBorder = BorderFactory.createTitledBorder("JPANEL TWO");
+            PanelTwo.setBorder(JPanelTwoBorder);
+            PanelTwo.setBackground(Color.MAGENTA.brighter());
+            return PanelTwo;
+        }
+
+        public JPanel CreateJPanelThree(){
+            PanelThree = new JPanel();
+            Border JPanelThreeBorder = BorderFactory.createTitledBorder("JPANEL THREE");
+            PanelThree.setBorder(JPanelThreeBorder);
+            PanelThree.setBackground(Color.GRAY.brighter());
+            return PanelThree;
+        }
+
+    }
